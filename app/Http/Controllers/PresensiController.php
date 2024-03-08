2de86016602c683b45b722aebf3a75f099d71ed1
rickyalfin <rickyalfina30@gmail.com>
2024-03-08 22:43:10 +0700

    Membuat design halaman presensi dan menampilkan peta lokasi user

---
+++ app/Http/Controllers/PresensiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PresensiController extends Controller
+{
+    public function create()
+    {
+        return view('presensi.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PresensiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,7 @@ Route::middleware(['guest:karyawan'])->group(function () {
 Route::middleware(['auth:karyawan'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/proseslogout', [AuthController::class, 'proseslogout']);
+
+    // Presensi
+    Route::get('/presensi/create', [PresensiController::class, 'create']);
 });
